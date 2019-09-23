91a2f8020569187b48807c3a433de2ddbcd371f6
Guillaume Lemer <guillaume@lemer.be>
2019-09-23 11:29:22 +0200

    mise en forme fonction random doc

--- formule1.c
+++ formule1.c
@@ -15,19 +15,36 @@
 int carList[] = {7, 99, 5, 16, 8, 20, 4, 55, 10, 26, 44, 77, 11, 18, 23, 33, 3, 27, 63, 88};
 double mainBoard[sizeof(carList)];
 
-
-int main(){
-
-    int lap[] = getLapTime();
-    printf(lap[0] + " - " + lap[1] + " - " + lap[2] + " - " + lap[3]);
-    return 0; // fin du programme
+/**
+ * renvois un nombre compris entre 40 et 45 secondes pour le
+ * clacul du temps des tours de circuits
+ * @return integer between 40 and 45
+ */
+int getRandomTime(){
+    return 45 - (rand() % 6)
 }
 
-int getLapTime() {
-    int lap[] = {45 - (rand() % 6), 45 - (rand() % 6), 45 - (rand() % 6), 0};
+/**
+ * génération d'un tableau conprenand le temps des sections et du temps total
+ * pour les voitures sous forme de pointeur
+ * @return array int *
+ */
+int * getLapTime(){
+    int lap[] = {getRandomTime(), getRandomTime(), getRandomTime(), 0};
     lap[3] = lap[0] + lap[1] + lap[2];
 
     return lap;
+}
+
+/**
+ * fonction principal du programme
+ * @return 0 => quit
+ */
+int main(){
+    int * lap;
+    lap = getLapTime();
+    printf(lap[0] , " - " , lap[1] , " - " , lap[2] , " - " , lap[3]);
+    return 0; // fin du programme
 }
 
 /*
