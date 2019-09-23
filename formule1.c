9d034bc5ddb136d6d382a121f30eec6fa81a3270
Guillaume Lemer <guillaume@lemer.be>
2019-09-23 15:00:47 +0200

    TP1 V2
    ajout fonction création tour des voitures
    ajout fonction affichage
    ajout compentaires

--- formule1.c
+++ formule1.c
@@ -16,7 +16,8 @@
 #include <stdlib.h>
 
 int carList[] = {7, 99, 5, 16, 8, 20, 4, 55, 10, 26, 44, 77, 11, 18, 23, 33, 3, 27, 63, 88};
-double mainBoard[sizeof(carList)];
+int mainBoard[sizeof(carList)][5]; // tableau global des voitures et des temps
+int * lap;
 
 /**
  * renvois un nombre compris entre 40 et 45 secondes pour le
@@ -24,7 +25,7 @@ double mainBoard[sizeof(carList)];
  * @return integer between 40 and 45
  */
 int getRandomTime(){
-    return 45 - (rand() % 9);
+    return 45 - (rand() % 9); // gestion du random et du temps perdu
 }
 
 /**
@@ -40,16 +41,47 @@ int * getLapTime(){
 }
 
 /**
+ * fonction de génération d'un tour de chacune des voitures par secteurs et total
+ * @return void
+ */
+void genOneTurn(){
+    for (int i = 0; i < sizeof(carList); i++){ // pour la liste des voitures
+        mainBoard[i][0] = carList[i];
+        lap = getLapTime(); // génération des secteurs et tu total pour la voiture
+        for (int j = 0; j < sizeof(lap); j++){ // remplissage du mainboard des lap pour les voitures
+            mainBoard[i][j+1] = lap[j];
+        }
+    }
+}
+
+/**
+ * affichage en console du tour qui vient d'etre effectué
+ */
+void showTurn(){
+    for (int i = 0; i < sizeof(mainBoard[0]) ; i++) {
+        printf("La voiture numéro %d ces temps de segments et tour sont : \n", mainBoard[i][0]);
+        for (int j = 1; j <= sizeof(mainBoard[i][0]); j++){
+            if(j == sizeof(mainBoard[i][0])){
+                printf("Total : %d\n", mainBoard[i][j]);
+            }
+            else {
+                printf("Secteur %d : %d\n",j,mainBoard[i][j]);
+            }
+        }
+    }
+}
+
+/**
  * fonction principal du programme
  * @return 0 => quit
  */
 int main(){
-    srand(time(NULL)); // initalision unique du random
+    srand(time(NULL)); // init unique du random
 
-    int * lap;
-    lap = getLapTime();
-    printf("Segment 1: %d Segment 2 : %d Segment 3: %d || Total : %d ",lap[0], lap[1] , lap[2] , lap[3]);
-    return 0; // fin du programme
+    genOneTurn();
+    showTurn();
+
+    return 0; // fin du programme - logout de la console
 }
 
 /*
